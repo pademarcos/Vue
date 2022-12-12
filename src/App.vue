694a7b101f22790f8186d9184bd33a9fc38072a1
pademarcos <pademarcos@gmail.com>
2022-12-12 19:19:11 -0300

    Cli funcionando

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,28 @@
 <template>
-    <div id="app">
-      <img alt="Vue logo" class="logo-small" src="./assets/logo.jpg">
-      <formulario-web />
-    </div>
-  </template>
-  
-  <script>
-  import FormularioWeb from './components/FormularioWeb.vue'
-  
-  export default {
-    name: 'App',
-    components: {
-      FormularioWeb
-    }
+  <div id="app">
+    <img alt="coder logo" class="logo-small" src="./assets/logo.jpg">
+    <formulario-web />
+  </div>
+</template>
+
+<script>
+import FormularioWeb from './components/FormularioWeb.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FormularioWeb
   }
-  </script>
-  
-  <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-  </style>
-  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
