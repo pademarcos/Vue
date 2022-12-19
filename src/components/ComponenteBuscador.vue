4843047d9ce342985020023ab03ce957324f6633
pademarcos <pademarcos@gmail.com>
2022-12-19 20:21:19 -0300

    buscador reparado

--- src/components/ComponenteBuscador.vue
+++ src/components/ComponenteBuscador.vue
@@ -38,14 +38,36 @@
 export default {
   name: 'ComponenteBuscador',
 
-  data: function () {
+  data:
+      
+  function () {
       return {
+        pacientes:[
+          {
+              socio:"1234",
+              nombre:"Juan Perez",
+              telefono:"1111111",
+              email:"4567@pepe.com"
+          },
+          {
+              socio:"3456",
+              nombre:"Marina Gomez",
+              telefono:"11222222",
+              email:"4567@pepe.com"
+          },
+          {
+              socio:"5678",
+              nombre:"Ignacio Hernandez",
+              telefono:"1113333",
+              email:"4567@pepe.com"
+          }
+      ],
         buscado: "",
         encontrado: "",
       };
     },
     props: {
-      pacientes: Array,
+      
       estilo: String,
     },
 
